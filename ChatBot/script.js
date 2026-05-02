57a49ef5dae433db6a66e2f236a7a298e8ac4a45
Eshan Shukla <eshukla15@gmail.com>
2026-05-02 09:54:33 +0530

    Update script.js

--- ChatBot/script.js
+++ ChatBot/script.js
@@ -8,8 +8,7 @@ const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
-const API_KEY = "AIzaSyDKP_tbIPwsz5ehj5UZeQ-tD7n4owVgwrE";
-const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key=${API_KEY}`;
+
 
 // Initialize user message and file data
 const userData = {
